dcd34a1fec7a833c3252ca6f9a11c29494b34177
Rajan-tharu <razanchy32@gmail.com>
2025-03-10 11:54:10 +0545

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,39 @@
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <a href="tel:1234567890"><i class="fas fa-phone"></i> +9779810233004</a>
+         <a href="tel:1234567890"><i class="fas fa-phone"></i> +9779828039649</a>
+         <a href="mailto:razanchy32@gmail.com"><i class="fas fa-envelope"></i> razanchy32@gmail.com</a>
+         <a href="mailto:razanchy07@gmail.com"><i class="fas fa-envelope"></i> razanchy07@gmail.com</a>
+         <a href="https://www.google.com/maps/@27.6955136,85.311488,13z"><i class="fas fa-map-marker-alt"></i>  Kathmandu 44600</a>
+      </div>
+
+      <div class="box">
+         <a href="#home">Home</a>
+         <a href="#about">About</a>
+         <a href="bookings.php">My Bookings</a>
+         <a href="#reservation">Reservation</a>
+         <a href="#gallery">Gallery</a>
+         <a href="#contact">Contact</a>
+         <a href="#reviews">Reviews</a>
+      </div>
+
+      <div class="box">
+         <a href="https://www.facebook.com/">facebook <i class="fab fa-facebook-f"></i>Ra-zan Chy</a>
+         <a href="https://twitter.com/">twitter <i class="fab fa-twitter"></i>Rajan tharu</a>
+         <a href="https://www.instagram.com/razan_chy/">instagram <i class="fab fa-instagram"></i>razan_chy</a>
+         
+         <a href="https://www.youtube.com/@rajantharu3292/">youtube <i class="fab fa-youtube"></i>Rajan Tharu</a>
+      </div>
+
+   </div>
+
+   <div class="credit">&copy; copyright @ 2025 by Rajan Tharu | all rights reseved!</div>
+
+</section>
+
+<!-- footer section ends -->
